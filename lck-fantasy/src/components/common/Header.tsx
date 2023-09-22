2359061f31010a4c4f6266224fe5c5db97ba4c7a
Yeji Jung <yzii9841@gmail.com>
2023-09-22 23:07:17 +0900

    FE:style: apply Header css

--- lck-fantasy/src/components/common/Header.tsx
+++ lck-fantasy/src/components/common/Header.tsx
@@ -1,6 +1,8 @@
+import styles from './Header.module.scss'
+
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <nav>HEADER</nav>
     </header>
   )
