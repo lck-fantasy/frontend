8f690e16edf3019334cae9e6533e884ef3a02d62
Yeji Jung <yzii9841@gmail.com>
2023-10-12 00:15:50 +0900

    Style:Home background

--- lck-fantasy/src/app/page.tsx
+++ lck-fantasy/src/app/page.tsx
@@ -1,16 +1,23 @@
+import styles from './home.module.scss'
+
 import Banner from '@/components/home/Banner'
 import PopularPlayer from '@/components/home/PopularPlayer'
 import PlayerRank from '@/components/home/PlayerRank'
 import TeamUserRank from '@/components/home/TeamUserRank'
 import Match from '@/components/home/Match'
+
 export default function Home() {
   return (
-    <>
+    <div className={styles.home}>
       <Banner />
       <PopularPlayer />
       <PlayerRank />
       <TeamUserRank />
       <Match />
-    </>
+      <span className={styles['home__bg1']} />
+      <span className={styles['home__bg2']} />
+      <span className={styles['home__bg3']} />
+      <span className={styles['home__bg4']} />
+    </div>
   )
 }
