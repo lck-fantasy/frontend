7ed88aab4cf418bfbb5d7330701bfde847980eab
Yeji <yzii9841@gmail.com>
2023-11-29 23:39:55 +0900

    FE:fix: remove RankBox from Match component

--- lck-fantasy/src/app/(home)/_Match/Match.tsx
+++ lck-fantasy/src/app/(home)/_Match/Match.tsx
@@ -1,9 +1,3 @@
-import ListBox from '@/components/common/RankBox'
-
 export default function Match() {
-  return (
-    <section>
-      <ListBox title={'경기 일정'} url={'#'} />
-    </section>
-  )
+  return <section>경기일정</section>
 }
