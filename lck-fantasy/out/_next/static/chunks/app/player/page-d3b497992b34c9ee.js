ab613c95bef36a69ef3c49f548c6c21497888d9d
Yeji <yzii9841@gmail.com>
2023-11-30 13:03:04 +0900

    deploy to gh-pages

---
+++ lck-fantasy/out/_next/static/chunks/app/player/page-d3b497992b34c9ee.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[633],{2990:function(n,e,u){Promise.resolve().then(u.t.bind(u,9630,23))},9630:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=2990)}),_N_E=n.O()}]);
